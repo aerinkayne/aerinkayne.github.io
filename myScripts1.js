31099c9cf5bcc1b744d4ddd3f02531fbd4ad9bd7
aerinkayne <39535726+aerinkayne@users.noreply.github.com>
2018-05-24 22:43:16 -0400

    Create myScripts1.js

---
+++ myScripts1.js
@@ -0,0 +1,27 @@
+
+		//header nav function for dropdowns
+		function dropList1() {
+			var x = document.getElementById("drop1");
+			if (x.style.display === "none") {
+				x.style.display = "flex";
+					} else {
+					x.style.display = "none";
+					}
+		}
+		
+		function dropList2() {
+			var x = document.getElementById("drop2");
+			if (x.style.display === "none") {
+				x.style.display = "block";
+					} else {
+					x.style.display = "none";
+					}
+		}
+		function dropList3() {
+			var x = document.getElementById("drop3");
+			if (x.style.display === "none") {
+				x.style.display = "block";
+					} else {
+					x.style.display = "none";
+					}
+		}
