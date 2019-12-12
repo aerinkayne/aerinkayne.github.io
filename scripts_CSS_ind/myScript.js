023191e5d59ec6bfe945b7f5fa38cd6aabd0903a
aerinkayne <39535726+aerinkayne@users.noreply.github.com>
2019-12-12 09:25:21 -0500

    Add files via upload

--- scripts_CSS_ind/myScript.js
+++ scripts_CSS_ind/myScript.js
@@ -1,13 +1,36 @@
-let elBurger = document.getElementById('burger');
-let elNavLinks = document.getElementById('navLinks');
-let themeOptions = document.getElementById('gridTheme'); 
 let elGridDisplay = document.getElementsByClassName('gridDisplay');
 elGridDisplay[0].style.display = "grid";
 
-elBurger.addEventListener('click', ()=> {
-	elBurger.classList.toggle('burgX');
-	elNavLinks.classList.toggle('showNav');
-})
+let elBurger = document.getElementById('burger');
+let elNavLinks = document.getElementById('navLinks');
+let themeOptions = document.getElementById('gridTheme'); 
+let dropBtns = document.getElementsByClassName('dropdown'); //there's only 1
+
+
+//IE why do you hate us?
+function getTarget(e) {
+	if (!e) {
+		e = window.event;
+	}
+	return e.target || e.srcElement;
+}
+
+//first li within ul#navLinks
+dropBtns[0].addEventListener('click', (e)=> {
+	let target = getTarget(e);
+	let children = target.childNodes; 
+	let drop;  //need to change display property of this element
+	for (let i = 0; i < children.length; i ++) {  
+		if (children[i].className === "dropdownContent") {  //use a param later 
+			drop = children[i];  //get correct element amidst whitespace nodes
+		}
+	}
+	if (drop.style.display === "none") {  
+		drop.style.display = "block";  //hope that the hover in CSS doesn't break the &world
+		} else {
+		drop.style.display = "none";
+		}	
+})	
 
 //uses radio to change the class of element with the #gridDisplay id.  
 themeOptions.addEventListener('change',()=> {
@@ -25,4 +48,8 @@ themeOptions.addEventListener('change',()=> {
 	}
 })	
 
+elBurger.addEventListener('click', ()=> {
+	elBurger.classList.toggle('burgX');
+	elNavLinks.classList.toggle('showNav');
+})
 
