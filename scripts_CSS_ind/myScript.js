931c3795342079f18e3aeaa2977c89e320a953ed
Carrie <chanscom7@gmail.com>
2020-05-13 13:43:09 -0400

    style and folder revisions

--- scripts_CSS_ind/myScript.js
+++ scripts_CSS_ind/myScript.js
@@ -1,53 +1,78 @@
 let elBurger = document.getElementById('burger');
 let elNavLinks = document.getElementById('navLinks');
-let elDropBtn1 = document.getElementById('dropBtn1');
-let elDrop1Content = document.getElementById('drop1Content');
-
-let waterItems;
-let elGridDisplay = document.getElementsByClassName('gridDisplay');
-let themeOptions = document.getElementById('gridTheme'); 
-
+let elDropBtns = document.getElementsByClassName('dropdown');
 
 elBurger.addEventListener('click', ()=> {
 	elBurger.classList.toggle('burgX');
 	elNavLinks.classList.toggle('showNav');
 })
 
-elDropBtn1.addEventListener('click', ()=> {
-	if (elDrop1Content.style.display === "none") {  
-		elDrop1Content.style.display = "block";  
-	} else {
-		elDrop1Content.style.display = "none";
-	}	
-})
-
-
-if (elGridDisplay[0]){
-	elGridDisplay[0].style.display = "grid";
-	waterItems = document.getElementById('sandWater').getElementsByClassName('gridItem');
-		for (let i=0; i< waterItems.length; i++){
-			waterItems[i].addEventListener('click', ()=> {
-				waterItems[i].style.transform = (waterItems[i].style.transform === 'rotateY(180deg)') ? 'rotateY(0deg)' : 'rotateY(180deg)';
-			})
-	}
-}
-
-//uses radio to change the class of element with the #gridDisplay id.  
-if(themeOptions){
-	themeOptions.addEventListener('change',()=> {
-		let theme;
-		for (let i = 0; i < themeOptions.length; i++){
-			for (let j = 0; j< elGridDisplay.length; j++){
-				//if radio btn checked and grid id is equal to btn's value, set display to grid
-				if (themeOptions[i].checked && elGridDisplay[j].id === themeOptions[i].value){ 
-					elGridDisplay[j].style.display = "grid";
-				}
-				else if (themeOptions[i].checked && elGridDisplay[j].id !== themeOptions[i].value){
-					elGridDisplay[j].style.display = "none";
-				}
-			}
-		}
-	})	
+if (elDropBtns.length){
+ for (let i = 0; i < elDropBtns.length; i++){
+  elDropBtns[i].addEventListener('click', (e)=> {
+   if (e.currentTarget.lastElementChild.style.display === "none") {  
+    e.currentTarget.lastElementChild.style.display = "block";  
+   } else {
+    e.currentTarget.lastElementChild.style.display = "none";
+   }	
+  })
+ }
 }
 
 
+let themeOptions = document.getElementsByName('theme'); 
+
+if (themeOptions.length){  //'theme' radio group used for sandWater, gridDisplay and gridItem
+ //ev on water grid
+ let waterItems = document.getElementById('sandWater').getElementsByClassName('gridItem');
+ for (let i=0; i< waterItems.length; i++){
+  waterItems[i].addEventListener('click', ()=> {
+   waterItems[i].style.transform = (waterItems[i].style.transform === 'rotateY(180deg)') ? 'rotateY(0deg)' : 'rotateY(180deg)';
+  })
+ }
+ //show initial grid
+ let elGridDisplay = document.getElementsByClassName('gridRadioDisplay');
+ elGridDisplay[0].style.display = "grid";
+ //radio val === display id
+ for (let i = 0; i < themeOptions.length; i++){
+  themeOptions[i].addEventListener('click',(e)=> {
+   for (let j = 0; j< elGridDisplay.length; j++){
+    elGridDisplay[j].style.display = "none";  
+   }
+   let selected = document.getElementById(e.currentTarget.value);
+   selected.style.display = "grid";
+  })	
+ }
+}
+
+
+
+let elFrameOptions = document.getElementsByName('frameOptions');//radio group name
+
+if(elFrameOptions.length){  
+ let divMaskList = document.getElementsByClassName('maskType'); //mask containers to change display of
+ divMaskList[0].style.display = "flex";                         //show one initially
+ for (let i = 0; i < elFrameOptions.length; i++)                    
+ elFrameOptions[i].addEventListener('click',(e)=> {  
+  for (let j = 0; j < divMaskList.length; j++){
+   divMaskList[j].style.display = "none";                       //reset all to display none
+  }
+  let maskT = document.getElementById(e.target.value);          //input val is same as mask container ID
+  maskT.style.display = "flex";                          
+ })
+}
+
+
+
+/*
+let imageOptions = document.getElementsByName('imageChoices');
+
+if(imageOptions.length){
+ imageOptions.addEventListener('click', (e)=> {
+  for (let i=0; i< divMaskList.length; i++){
+   divMaskList[i].classList.toggle(e.target.value);
+  }
+  
+ });
+}
+//*/
